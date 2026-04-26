baa4e4a87ad19f67dd200186bacbe41cfc844ae4
LipartelianiNiko <niko.liparteliani.dev@gmail.com>
2026-04-26 13:21:24 +0400

    chore: minor addition to User class

--- JobBoard/Models/User.cs
+++ JobBoard/Models/User.cs
@@ -4,12 +4,17 @@
     public class User
     {
         //each user has id, email, passwor, role.both roles have this, but employer doesnt need name at all.
-        public int Id { get; set; } 
+        public int Id { get; set; }
         public string Email { get; set; } = string.Empty;
-
         public string PasswordHash { get; set; } = string.Empty;
         public Role Role { get; set; }
-        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        //each user is either employer or seeker. 
+        //user object is same for each role but additional profile object is created accroding to the role.
+        //user object holds its coresponding role profile object, "?" indicates that attribute can be null.
+        public SeekerProfile? SeekerProfile { get; set; }
+        public EmployerProfile? EmployerProfile { get; set; }
 
     }
 }
