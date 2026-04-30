99b8e1ca4e69a360399864252cd5ca96187f1c6b
LipartelianiNiko <niko.liparteliani.dev@gmail.com>
2026-04-30 10:08:15 +0400

    added JwtHelper for token generation

---
+++ JobBoard/Helpers/JwtHelper.cs
@@ -0,0 +1,40 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using JobBoard.Models;
+
+
+
+namespace JobBoard.Helpers
+{
+    public class JwtHelper
+    {
+        private readonly string _secretKey;
+
+        public JwtHelper(IConfiguration configuration)
+        {
+            _secretKey = configuration["Jwt:Key"]!;
+        }
+
+        public string GenerateToken(User user)
+        {
+            var claims = new[]
+            {
+                new Claim("userId", user.Id.ToString()),
+                new Claim(ClaimTypes.Role, user.Role.ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secretKey));
+            var credentials=new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(7),
+                signingCredentials: credentials
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+        }
+    }
+}
