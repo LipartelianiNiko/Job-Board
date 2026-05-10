d1a66b123594b67e1e55232ef5cdb6212a80d497
LipartelianiNiko <niko.liparteliani.dev@gmail.com>
2026-05-10 16:35:29 +0400

    chore: added automatic migration in program.cs for railwy deployement

--- JobBoard/Program.cs
+++ JobBoard/Program.cs
@@ -61,5 +61,12 @@ app.UseAuthentication();//adds jwt middleware, ceck token on each request
 app.UseAuthorization();
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
+app.Run();
 
 app.Run();//start the server
